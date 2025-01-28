e745e953c45ce4663aa6cd434cd69817135e723a
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 23:04:43 +0530

    Add files via upload
    this program displays the multiplication table of a number

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int n= 5;
+
+        System.out.println("Multiplication table of " + n + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n+ " x " + i + " = " + (n * i));
+        }
+    }
+}
+
+
